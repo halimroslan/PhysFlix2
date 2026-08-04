06c8456fa19b6f5b81bec5afdb2c24304d5165f1
halimroslan <abdulhalimroslan@gmail.com>
2026-08-04 22:55:32 +0800

    Configure Firebase Auth for project Firebase 1 (gen-lang-client-0414668770)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,11 +3,11 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBS3MFGgLmISPmLD96qt02MT62IJsMBDio",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "physicsspmflix.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "physicsspmflix",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "physicsspmflix.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "102938475610",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:102938475610:web:83921049201"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "gen-lang-client-0414668770.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "gen-lang-client-0414668770",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "gen-lang-client-0414668770.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "414396564667",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:414396564667:web:83921049201"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();

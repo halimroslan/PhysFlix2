beba118fc2c73c91ab20610b2330a2888bf34478
halimroslan <abdulhalimroslan@gmail.com>
2026-08-07 14:21:00 +0800

    Fix video player aspect ratio and DRM protectors for mobile

--- src/components/VideoPlayerView.tsx
+++ src/components/VideoPlayerView.tsx
@@ -239,12 +239,8 @@ export const VideoPlayerView: React.FC<VideoPlayerViewProps> = ({
           {/* DRM Video Container with Anti-Screen Capture Watermark & Protected Overlays */}
           <div
             onContextMenu={(e) => e.preventDefault()}
-            className="relative w-full mx-auto overflow-hidden bg-black border border-slate-800 shadow-2xl group select-none"
-            style={{ 
-              aspectRatio: '16/9', 
-              maxHeight: '75vh', 
-              maxWidth: 'calc(75vh * 16 / 9)' 
-            }}
+            className="relative w-full mx-auto overflow-hidden bg-black border border-slate-800 shadow-2xl group select-none rounded-xl"
+            style={{ paddingBottom: '56.25%' }}
           >
             {/* Embedded Stream via Obfuscated ID */}
             <iframe
@@ -277,15 +273,15 @@ export const VideoPlayerView: React.FC<VideoPlayerViewProps> = ({
             {showTavisM1toM20 && (
               <>
                 {/* Top-Right Tavis Protector (M1-M20) */}
-                <div className="absolute top-[14%] right-[24%] z-20 flex items-center justify-center w-24 lg:w-36 h-8 lg:h-12 bg-[#0a0a0a] rounded-lg lg:rounded-xl shadow-xl border border-white/10 pointer-events-none">
+                <div className="absolute top-[18%] md:top-[14%] right-[5%] md:right-[24%] z-20 flex items-center justify-center w-16 md:w-24 lg:w-36 h-6 md:h-8 lg:h-12 bg-[#0a0a0a] rounded-md lg:rounded-xl shadow-xl border border-white/10 pointer-events-none">
                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-3 lg:h-5 w-auto object-contain" />
+                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-2 md:h-3 lg:h-5 w-auto object-contain" />
                 </div>
                 
                 {/* Bottom-Right Tavis Protector (M1-M20) */}
-                <div className="absolute bottom-[10%] right-[24%] z-20 flex items-center justify-center w-24 lg:w-36 h-8 lg:h-12 bg-[#0a0a0a] rounded-lg lg:rounded-xl shadow-xl border border-white/10 pointer-events-none">
+                <div className="absolute bottom-[20%] md:bottom-[15%] right-[5%] md:right-[24%] z-20 flex items-center justify-center w-16 md:w-24 lg:w-36 h-6 md:h-8 lg:h-12 bg-[#0a0a0a] rounded-md lg:rounded-xl shadow-xl border border-white/10 pointer-events-none">
                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-3 lg:h-5 w-auto object-contain" />
+                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-2 md:h-3 lg:h-5 w-auto object-contain" />
                 </div>
               </>
             )}
@@ -293,15 +289,15 @@ export const VideoPlayerView: React.FC<VideoPlayerViewProps> = ({
             {showTavisM21Plus && (
               <>
                 {/* Top-Left Tavis Protector (M21+) */}
-                <div className="absolute top-[6%] left-[2%] z-20 flex items-center justify-center w-20 lg:w-32 h-6 lg:h-10 bg-[#0a0a0a] rounded-md lg:rounded-lg shadow-xl border border-white/10 pointer-events-none">
+                <div className="absolute top-[10%] md:top-[6%] left-[5%] md:left-[2%] z-20 flex items-center justify-center w-16 md:w-20 lg:w-32 h-5 md:h-6 lg:h-10 bg-[#0a0a0a] rounded-sm lg:rounded-lg shadow-xl border border-white/10 pointer-events-none">
                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-2.5 lg:h-4 w-auto object-contain" />
+                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-2 md:h-2.5 lg:h-4 w-auto object-contain" />
                 </div>
                 
                 {/* Top-Right Tavis Protector (M21+) */}
-                <div className="absolute top-[5%] right-[18%] z-20 flex items-center justify-center w-28 lg:w-40 h-8 lg:h-14 bg-[#0a0a0a] rounded-md lg:rounded-lg shadow-xl border border-white/10 pointer-events-none">
+                <div className="absolute top-[10%] md:top-[5%] right-[5%] md:right-[18%] z-20 flex items-center justify-center w-20 md:w-28 lg:w-40 h-6 md:h-8 lg:h-14 bg-[#0a0a0a] rounded-sm lg:rounded-lg shadow-xl border border-white/10 pointer-events-none">
                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-3 lg:h-6 w-auto object-contain" />
+                   <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix" className="h-2 md:h-3 lg:h-6 w-auto object-contain" />
                 </div>
               </>
             )}

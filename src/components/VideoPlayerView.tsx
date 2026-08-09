f6346c7e274ab4b3faa6f4d25f60f26cabcc554c
halimroslan <abdulhalimroslan@gmail.com>
2026-08-10 07:40:49 +0800

    Increase size of global watermark to 95%

--- src/components/VideoPlayerView.tsx
+++ src/components/VideoPlayerView.tsx
@@ -571,7 +571,7 @@ export const VideoPlayerView: React.FC<VideoPlayerViewProps> = ({
                     height: '23.076%' // Span 6 rows (I to N)
                   }}
                 >
-                  <div className="bg-slate-900 rounded-2xl p-3 md:p-5 opacity-40 scale-[0.70] h-full flex items-center justify-center">
+                  <div className="bg-slate-900 rounded-2xl p-3 md:p-5 opacity-40 scale-[0.95] h-full flex items-center justify-center">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img src="/PHYSFLIX.png" alt="PhysicsSPMFlix Watermark" className="h-full w-auto object-contain" />
                   </div>
